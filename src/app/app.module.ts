a09598bd9698f1f402a16d5b363e8519a43c030f
julioallday <109560993+julioallday@users.noreply.github.com>
2022-12-25 04:52:28 +0000

    Criação de navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BlogPostComponent
+    BlogPostComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
